3efe9898d2ae65a4a2c02db7da2afd287339bbf6
ronan <ronanlf12@gmail.com>
2023-06-22 16:06:06 -0400

    escrita de sql das tabelas usadas nos primeiros módulos

---
+++ Back-End/database/bd.sql
@@ -0,0 +1,46 @@
+CREATE TABLE nivel_permissao(
+   id SERIAL PRIMARY KEY,
+   nome_nivel VARCHAR(150) NOT NULL,
+   criado_em timestamp without time zone DEFAULT now(),
+   atualizado_em timestamp without time zone DEFAULT now()
+);
+
+CREATE TABLE usuario(
+   id SERIAL PRIMARY KEY,
+   nivel_permissao_id INTEGER NOT NULL,
+   nome_usuario VARCHAR(150) NOT NULL,
+   cpf CHAR(14),
+   senha VARCHAR(255) NOT NULL,
+   email_usuario VARCHAR(150) NOT NULL,
+   ativo BOOLEAN DEFAULT TRUE,
+   criado_em timestamp without time zone DEFAULT now(),
+   atualizado_em timestamp without time zone DEFAULT now(),
+   FOREIGN KEY(nivel_permissao_id) REFERENCES nivel_permissao(id)
+);
+
+CREATE TABLE estabelecimento(
+   id SERIAL PRIMARY KEY,
+   nome_estabelecimento VARCHAR(150) NOT NULL,
+   endereco_bairro
+   endereco_numero
+   endereco_logradouro
+   endereco_nome_logradouro VARCHAR(150)
+   endereco_cidade VARCHAR(100)
+   endereco_estado VARCHAR(100)
+   endereco_complemento TEXT,
+   endereco_cep CHAR(9),
+   ativo BOOLEAND DEFAULT TRUE,
+   visivel_agendamento BOOLEAN NOT NULL,
+   horario_abertura TIME NOT NULL,
+   horario_fechamento_almoco TIME,
+   horario_volta_almoco TIME,
+   horario_fechamento TIME NOT NULL,
+   fechamento_almoco BOOL NOT NULL,
+   criado_em timestamp without time zone DEFAULT now(),
+   atualizado_em timestamp without time zone DEFAULT now()
+);
+
+CREATE TABLE estabelecimento_has_usuario(
+   estabelecimento_id INTEGER NOT NULL,
+   usuario_id INTEGER NOT NULL
+);
