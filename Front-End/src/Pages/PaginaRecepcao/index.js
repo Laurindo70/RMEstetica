b96241b74521763d753d7845fbf57a438b88932f
Ronan Laurindo Flor <ronanlf12@gmail.com>
2023-11-15 14:23:09 -0400

    tela inicial finalizada

--- Front-End/src/Pages/PaginaRecepcao/index.js
+++ Front-End/src/Pages/PaginaRecepcao/index.js
@@ -1,31 +1,65 @@
 import React from 'react';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
-const { Header, Content, Footer } = Layout;
+import { Layout, Menu, Flex, Typography, Row, Button, Dropdown } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import imagemInicio from '../../Images/img-inicio-central.svg';
+import './style.css';
+
+const { Header, Content } = Layout;
+const items = [
+   {
+      label: '1st menu item',
+      key: '1',
+   },
+   {
+      label: '2nd menu item',
+      key: '2',
+   },
+   {
+      label: '3rd menu item',
+      key: '3',
+   },
+];
 
 function PaginaRecepcao() {
-   const {
-      token: { colorBgContainer },
-   } = theme.useToken();
+   const navigate = useNavigate();
+
+   function mudarPagina(value) {
+      navigate(`${value.key}`);
+   }
+
    return (
       <Layout className="layout">
          <Header
             style={{
+               background: '#FE9CCC',
                display: 'flex',
                alignItems: 'center',
+               maxWidth: '100vw'
             }}
          >
             <div className="demo-logo" />
             <Menu
-               theme="dark"
+               style={{ background: '#FE9CCC', color: '#fff', fontWeight: 'bold' }}
                mode="horizontal"
-               defaultSelectedKeys={['2']}
-               items={new Array(15).fill(null).map((_, index) => {
-                  const key = index + 1;
-                  return {
-                     key,
-                     label: `nav ${key}`,
-                  };
-               })}
+               onClick={mudarPagina}
+               items={[
+                  {
+                     key: '/login',
+                     label: 'Login',
+                  },
+                  {
+                     key: '/register-user',
+                     label: 'Cadastrar-se como usuário',
+                  },
+                  {
+                     key: '/register-estabelecimento',
+                     label: 'Cadastrar-se como estabelecimento',
+                  },
+                  {
+                     key: '/agendamento',
+                     label: 'Agendamentos',
+                  }
+               ]}
             />
          </Header>
          <Content
@@ -33,31 +67,41 @@ function PaginaRecepcao() {
                padding: '0 50px',
             }}
          >
-            <Breadcrumb
-               style={{
-                  margin: '16px 0',
-               }}
-            >
-               <Breadcrumb.Item>Home</Breadcrumb.Item>
-               <Breadcrumb.Item>List</Breadcrumb.Item>
-               <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div
-               className="site-layout-content"
-               style={{
-                  background: colorBgContainer,
-               }}
-            >
-               Content
-            </div>
+
+            <Flex className='container-pagina-inicio'>
+               <div className='itens-inicio-1'>
+                  <Row>
+                     <Typography.Title level={2} style={{ color: '#FE9CCC', fontStyle: 'italic' }}>
+                        <span style={{ color: '#A9335D' }} >RME</span>stetica
+                     </Typography.Title></Row>
+                  <Row>
+                     <Typography.Title level={3} style={{ color: '#FE9CCC', fontStyle: 'italic' }}>
+                        Seja Bem-Vindo!
+                     </Typography.Title>
+                  </Row>
+                  <Row>
+                     <Typography.Title level={4} style={{ color: '#A9335D', fontStyle: 'italic' }}>
+                        Esse é o seu site de controle de estabelecimento
+                     </Typography.Title>
+                     <Typography.Title level={4} style={{ color: '#A9335D', fontStyle: 'italic' }}>
+                        e agendamento de procedimentos estéticos.
+                     </Typography.Title>
+                  </Row>
+                  <Row>
+                     <button className='botao-cadastro-inicio' onClick={() => { navigate("/register-user"); }} >Cadastrar-se como usuário</button>
+                     <button className='botao-cadastro-inicio' onClick={() => { navigate("/register-estabelecimento"); }}>Cadastrar-se como Estabelecimento</button>
+                     <button className='botao-cadastro-inicio' onClick={() => { navigate("/login"); }}>Realizar Login</button>
+                  </Row>
+                  <Row>
+                     <button className='botao-cadastro-inicio' onClick={() => { navigate("/register-user"); }} >Realizar Agendamento </button>
+                  </Row>
+               </div>
+               <div className='itens-inicio-2'>
+                  <img style={{ cursor:'pointer' }} onClick={() => { window.open("https://storyset.com/work"); }} src={imagemInicio}></img>
+               </div>
+            </Flex>
+
          </Content>
-         <Footer
-            style={{
-               textAlign: 'center',
-            }}
-         >
-            Ant Design ©2023 Created by Ant UED
-         </Footer>
       </Layout>
    );
 };
